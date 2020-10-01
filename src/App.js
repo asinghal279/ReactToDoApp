647445022c3a79b6fc09e80462bee889411282c6
Raghav Singhal <raghav.singhal@mountblue.tech>
2020-10-01 18:32:54 +0530

    Add the warning block functionality for not deleting the last project present in the list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React, { Component } from "react";
-import Project from './project';
+import Project from "./project";
 
 class App extends Component {
   state = {
-    projects: ["Async/Await", "EasyBank"]
+    projects: ["Async/Await", "EasyBank", "Standup", "GoodMorning"],
   };
 
   handleDeleteProject = (project) => {
-    let index = this.state.projects.indexOf(project) 
-    if(index>-1)
-    this.setState({
-      projects: this.state.projects.splice(index,1)
-    })
+    let index = this.state.projects.indexOf(project);
+    if (index > -1 && this.state.projects.length>1) {
+      this.state.projects.splice(index, 1);
+      this.setState(
+        {
+          projects: this.state.projects,
+        },
+        () => console.log(this.state.projects)
+      );
+      return true;
+    }
+    else{
+      return false
+    }
+  };
+
+  componentDidMount() {
+    document.querySelectorAll(".close").forEach((button) => {
+      button.addEventListener("click", () => {
+        button.parentElement.classList.add("d-none");
+      });
+    });
   }
+
   render() {
     return (
       <div className="container">
         <div className="row">
-          <Project list={this.state.projects} onDelete={(projectToDelete) => this.handleDeleteProject(projectToDelete)}/>
+          <Project
+            list={this.state.projects}
+            onDelete={(projectToDelete) =>
+              this.handleDeleteProject(projectToDelete)
+            }
+          />
         </div>
       </div>
     );
--- src/project.js
+++ src/project.js
@@ -7,8 +7,9 @@ class Project extends Component {
   };
 
   changeSelectedProject = (e) => {
+    let optionsNode = document.querySelector("#selector");
     let selectedProjectName =
-      e.target.options[e.target.selectedIndex].innerHTML;
+      optionsNode.options[optionsNode.selectedIndex].innerHTML;
     this.setState({ selectedProject: selectedProjectName }, () =>
       console.log(this.state.selectedProject)
     );
@@ -18,10 +19,10 @@ class Project extends Component {
     return (
       <div className="col-sm-6">
         <h2>Project's Selection</h2>
-        <div class="input-group col-sm-8 px-0">
+        <div class="input-group col-sm-8 px-0 mb-4">
           <select
             class="custom-select"
-            id="inputGroupSelect04"
+            id="selector"
             onChange={this.changeSelectedProject}
           >
             {this.state.projects.map((project) => (
@@ -29,11 +30,67 @@ class Project extends Component {
             ))}
           </select>
           <div class="input-group-append">
-            <button onClick={() => this.props.onDelete(this.state.selectedProject)} class="btn btn-outline-danger" type="button">
+            <button
+              onClick={async () => {
+                let bool = await this.props.onDelete(
+                  this.state.selectedProject
+                );
+                await this.changeSelectedProject();
+                if (!bool) {
+                  document.querySelector("#warning").classList.remove("d-none");
+                  document.querySelector("#warning").classList.add("show");
+                }
+              }}
+              class="btn btn-outline-danger"
+              type="button"
+            >
               Delete Project
             </button>
           </div>
         </div>
+        <div
+          class="alert alert-warning alert-dismissible fade d-none"
+          id="warning"
+          role="alert"
+        >
+          <strong>Warning !</strong> There should be atleast one project in the
+          list
+          <button type="button" class="close" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <form action="" class="col-sm-12 p-0 mt-4" id="projectForm">
+          <div class="form-group">
+            <h3 for="project">Please enter a new Project here :</h3>
+            <input
+              type="text"
+              name="project"
+              id="projectName"
+              class="form-control mb-4"
+              placeholder="Project Name"
+              required
+            />
+            {/* <div
+                class="alert alert-success alert-dismissible fade d-none"
+                id="projectAddSuccess"
+                role="alert"
+                data-hide="alert"
+              >
+                <strong> Project Added </strong> to the options list. Please go
+                and select your project from the list.
+                <button type="button" class="close" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div> */}
+            <div class="text-right">
+              <input
+                type="submit"
+                value="Add Project"
+                class="btn btn-outline-success btn-md"
+              />
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
