e9196953b4396dec788f78a7f3aa58764416bcd9
Raghav Singhal <raghav.singhal@mountblue.tech>
2020-10-03 15:15:43 +0530

    Run build command

---
+++ build/precache-manifest.2104248dc569c8eb6799f8eecfa28829.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68706446162e2fe649ef33a7ce95eb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001a39d8c87eebb6453d",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4ee797b0218822c6bd90",
+    "url": "/static/css/main.8f5daf99.chunk.css"
+  },
+  {
+    "revision": "001a39d8c87eebb6453d",
+    "url": "/static/js/2.fd4e5ad9.chunk.js"
+  },
+  {
+    "revision": "d81c6c91afd2bb4bf8caccddf0472235",
+    "url": "/static/js/2.fd4e5ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee797b0218822c6bd90",
+    "url": "/static/js/main.bbae9dbd.chunk.js"
+  },
+  {
+    "revision": "83092d25cdfc30edffe4",
+    "url": "/static/js/runtime-main.51dc1ee2.js"
+  }
+]);
